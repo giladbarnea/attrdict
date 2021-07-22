5bc1291ac232d4ca99b762b49dd887aee8a1b2a6
Gilad Barnea <cr-gbarn-herolo@allot.com>
2021-07-22 15:33:59 +0000

    Thu Jul 22 15:33:58 UTC 2021

--- diktionary/nested_dict.py
+++ diktionary/nested_dict.py
@@ -6,18 +6,14 @@
 #  - every read also writes
 class NestedDict(dict):
     def __getitem__(self, key):
-        # print(f'{self}.__getitem__({key = })')
         if key not in self:
             value = NestedDict()
             self.__setitem__(key, value)
-            # return self[key]
             return value
-            # return NestedDict()
-            # return self.setdefault(key, NestedDict())
         value = self.get(key)
         if isinstance(value, dict):
             value = NestedDict(value)
-            # self.__setitem__(key, value)
+            self.__setitem__(key, value)
         return value
 
     # def __setitem__(self, k, v) -> None:
